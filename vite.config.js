e76bd9057901e23e0ba736ea930a728210f0fed1
ExpGroza <expgroza@gmail.com>
2024-11-18 03:21:00 +0700

    12 commit

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        assetsDir: '',
-        manifest: true,
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-            },
-        },
-    },
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
 });
