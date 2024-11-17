041075c02d1afa4c82820e5fcd9aad5ec8c3929d
ExpGroza <expgroza@gmail.com>
2024-11-18 02:36:20 +0700

    6 comit

--- config/filament.php
+++ config/filament.php
@@ -6,18 +6,18 @@ return [
     'auth' => [
         'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
     ],
-    'middleware' => [
-        'base' => [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'middleware' => [
+            'base' => [
+                \Illuminate\Cookie\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\Session\Middleware\AuthenticateSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ],
+            'auth' => [
+                \Filament\Http\Middleware\Authenticate::class,
+            ],
         ],
-        'auth' => [
-            \Filament\Http\Middleware\Authenticate::class,
-        ],
-    ],
 ];
